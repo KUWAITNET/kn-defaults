c5af80ba8ba0635124eab8a0b005aaae196a5943
Ramez Issac <ramez@rasystems.io>
2020-06-02 10:33:30 +0200

    Update `cms_plugin_change_admin_log`

--- kn_defaults/logging/handlers.py
+++ kn_defaults/logging/handlers.py
@@ -2,17 +2,19 @@ from django.contrib.contenttypes.models import ContentType
 
 
 def cms_plugin_change_admin_log(sender, operation, request, language, token, origin, **kwargs):
+    from cms.models import CMSPlugin
     operation = operation
     data = ''
 
     from django.contrib.admin.models import LogEntry
     placeholder = kwargs['placeholder']
+    pk = placeholder.pk
     if 'change' in operation:
         action_flag = '2'
         data = {}
         old_plugin = kwargs['old_plugin'].__dict__
         new_plugin = kwargs['new_plugin'].__dict__
-
+        pk = new_plugin['id']
         for key, val in old_plugin.items():
             if not key.startswith('_') and not key == 'changed_date':
                 new_value = new_plugin.get(key)
@@ -26,8 +28,8 @@ def cms_plugin_change_admin_log(sender, operation, request, language, token, ori
 
     LogEntry.objects.create(
         user=request.user,
-        content_type=ContentType.objects.get_for_model(placeholder),
-        object_id=placeholder.pk,
+        content_type=ContentType.objects.get_for_model(CMSPlugin),
+        object_id=pk,
         object_repr=str(placeholder),
         action_flag=action_flag,
         change_message=f'{data}',
