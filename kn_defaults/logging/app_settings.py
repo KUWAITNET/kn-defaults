3c1c60da81ef3aff6358794f7f76de0ae12b3fad
Ramez Issac <ramez@rasystems.io>
2020-06-22 10:53:34 +0200

    WIP

--- kn_defaults/logging/app_settings.py
+++ kn_defaults/logging/app_settings.py
@@ -6,8 +6,17 @@ KN_LOGGING_URL_PATTERNS = lazy(lambda: getattr(settings, 'KN_LOGGING_URL_PATTERN
 DISABLE_CMS_PLUGIN_CHANGE_ADMIN_LOG = lazy(lambda: getattr(settings, 'DISABLE_CMS_PLUGIN_CHANGE_ADMIN_LOG', False),
                                            bool)()
 KN_LOG_FILE_SIZE = lazy(lambda: getattr(settings, 'KN_LOG_FILE_SIZE', 5 * 1024 * 1024), int)()
-KN_HANDLER_CLASS = lazy(
-    lambda: getattr(settings, 'KN_HANDLER_CLASS', 'logging.handlers.RotatingFileHandler'), str)()
-KN_LOG_FILE_PATH = lazy(
-    lambda: getattr(settings, 'KN_LOG_FILE_PATH', os.path.join(os.getcwd(), 'log.log')), str)()
+
+try:
+    KN_HANDLER_CLASS = lazy(
+        lambda: getattr(settings, 'KN_HANDLER_CLASS', 'logging.handlers.RotatingFileHandler'), str)()
+except:
+    KN_HANDLER_CLASS = 'logging.handlers.RotatingFileHandler'
+
+try:
+    KN_LOG_FILE_PATH = lazy(
+        lambda: getattr(settings, 'KN_LOG_FILE_PATH', os.path.join(os.getcwd(), 'log.log')), str)()
+except:
+    KN_LOG_FILE_PATH = os.path.join(os.getcwd(), 'log.log')
+
 KN_LOG_BACKUP_COUNT = lazy(lambda: getattr(settings, 'KN_LOG_BACKUP_COUNT', 3), int)()
