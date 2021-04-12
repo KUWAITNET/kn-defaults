f0262cb4401aa0ad75bf07f9c44429829e05e7ee
Ahmed ElBahnihi <abahnihi@gmail.com>
2021-04-12 13:12:35 +0200

    handle PUT request

--- kn_defaults/logging/middlewares.py
+++ kn_defaults/logging/middlewares.py
@@ -96,7 +96,11 @@ class KnLogging(MiddlewareMixin):
             return {}
         else:
             sensitive_post_parameters = getattr(request, 'sensitive_post_parameters', [])
-            method_attr = getattr(request, method, None)
+            if method == "PUT":
+                method_attr = json.loads(getattr(request, "body", b"{}"))
+            else:
+                method_attr = getattr(request, method, None)
+            
             if not method_attr:
                 return {}
             cleansed = method_attr.copy()
